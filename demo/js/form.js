3c6bb9a5326bd936056a9644c7c5dac396381182
Erin580066 <597516068@qq.com>
2016-12-19 22:35:27 +0800

    change12192235

--- demo/js/form.js
+++ demo/js/form.js
@@ -1,4 +1,29 @@
 angular.module('myapp',[])
 .controller('myCtrl',function($scope){
-	
+	$scope.hobby = [
+		{
+			id : 1,
+			name : '打游戏'
+		},
+		{
+			id : 2,
+			name : '敲代码'
+		},
+		{
+			id : 3,
+			name : 'KTV'
+		}
+	];
+	$scope.data = {
+		hobbies : [1,2]
+	}
+	$scope.toggleSelect = function(id){
+		var index = $scope.data.hobbies.indexOf(id);
+		if(index === -1){
+			$scope.data.hobbies.push(id);
+		}else{
+			$scope.data.hobbies.splice(index,1)
+		}
+		console.log($scope.data.hobbies)
+	}
 })
