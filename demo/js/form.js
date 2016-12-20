8f50adac71ef3a65a0911247fec6cea0f528e531
Erin580066 <597516068@qq.com>
2016-12-20 22:34:13 +0800

    change12202234

--- demo/js/form.js
+++ demo/js/form.js
@@ -1,4 +1,15 @@
 angular.module('myapp',[])
+.filter('cityFilter',function(){
+	return function(data,parent){
+		var filterData = [];
+		angular.forEach(data,function(obj){
+			if(obj.parent=== parent){
+				filterData.push(obj)
+			}
+		})
+		return filterData;
+	}
+})
 .controller('myCtrl',function($scope){
 	$scope.hobby = [
 		{
@@ -12,6 +23,72 @@ angular.module('myapp',[])
 		{
 			id : 3,
 			name : 'KTV'
+		}
+	];
+	$scope.cites = [
+		{
+			name : '上海',
+			parent : 0,
+			id : 1
+		},
+		{
+			name : '上海市',
+			parent : 1,
+			id : 2
+		},
+		{
+			name :'徐汇区',
+			parent : 2,
+			id : 8
+		},
+		{
+			name :'长宁区',
+			parent : 2,
+			id : 3
+		},{
+			name : '北京',
+			parent : 0,
+			id : 4
+		},
+		{
+			name : '北京市',
+			parent : 4,
+			id : 5
+		},
+		{
+			name :'海淀区',
+			parent : 5,
+			id : 6
+		},
+		{
+			name :'昌平区',
+			parent : 5,
+			id : 7
+		},
+		{
+			name : '浙江',
+			parent : 0,
+			id : 9
+		},
+		{
+			name :'杭州',
+			parent : 9,
+			id : 100
+		},
+		{
+			name :'宁波',
+			parent : 9,
+			id : 11
+		},
+		{
+			name :'西湖区',
+			parent : 100,
+			id : 12
+		},
+		{
+			name :'北仑区',
+			parent : 11,
+			id : 13
 		}
 	];
 	$scope.data = {
